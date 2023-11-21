d0428499dccf7a535405affe67e59ff1297fe71d
Evan Johns <evandjohns@gmail.com>
2023-11-20 23:47:07 -0500

    Added custom cost validations for product page

--- StockPro.CoreBusiness/Prodcut.cs
+++ StockPro.CoreBusiness/Prodcut.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using StockPro.CoreBusiness;
+using StockPro.CoreBusiness.Validations;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -18,7 +20,26 @@ namespace StockPro.CoreBusiness
         [Range(0, int.MaxValue, ErrorMessage = "Quantity must be greater or equal to 0")]
         public int Quantity { get; set; }
 
-        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be greater or equal to 0")]
+        [Range(0, int.MaxValue, ErrorMessage = "Price must be greater or equal to 0")]
         public double Price { get; set; }
+
+        [Product_EnsurePriceIsGreaterThanInventoriesCost]
+        public List<ProductInventory> ProductInventories { get; set; } = new List<ProductInventory>();
+
+        public void AddInventory(Inventory inventory)
+        {
+            if (!this.ProductInventories.Any(x => x.Inventory != null &&
+                x.Inventory.InventoryName.Equals(inventory.InventoryName)))
+            {
+                this.ProductInventories.Add(new ProductInventory
+                {
+                    InventoryId = inventory.InventoryId,
+                    Inventory = inventory,
+                    InventoryQuantity = 1,
+                    ProductId = this.ProductId,
+                    Product = this
+                });
+            }
+        }
     }
 }
